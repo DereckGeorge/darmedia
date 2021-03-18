ca5fcbc0e62f9354fef7833b3efd220b90f25c68
DereckGeorge <80265783+DereckGeorge@users.noreply.github.com>
2021-03-18 15:39:13 +0300

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -59,8 +59,7 @@ async def answer(bot, query):
 def get_reply_markup(username):
     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
     buttons = [[
-        InlineKeyboardButton('Search tena ', switch_inline_query_current_chat=''),
-        InlineKeyboardButton('Share kwa watu wengine', url=url),
+        
     ]]
     return InlineKeyboardMarkup(buttons)
 
